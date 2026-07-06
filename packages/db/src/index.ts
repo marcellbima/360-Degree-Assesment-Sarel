5b2f7263fd0066ab31f54e49aaa014e0cbac4d85
Marcellinus Bima <m.bima.engr@gmail.com>
2026-07-06 15:38:39 +0700

    feat(db): add PostgreSQL import lookup repository

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -29,3 +29,4 @@ export { PostgresHealthRepository } from './repositories/postgres-health-reposit
 export { PostgresRoleRepository } from './repositories/postgres-role-repository';
 export { PostgresAssessmentTypeRepository } from './repositories/postgres-assessment-type-repository';
 export { PostgresUserRepository } from './repositories/postgres-user-repository';
+export { PostgresImportLookupRepository } from './repositories/postgres-import-lookup-repository';
